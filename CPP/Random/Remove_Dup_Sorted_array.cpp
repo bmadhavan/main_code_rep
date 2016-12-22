9e450973fb6f50abf3238ef25224456ba328bb62
bmadhavan <balasubramanian.madhavan@gmail.com>
2016-12-21 23:58:23 -0800

    Remove dup

---
+++ CPP/Random/Remove_Dup_Sorted_array.cpp
@@ -0,0 +1,25 @@
+Remove Duplicates from Sorted Array
+
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       int first, sub, res=0;
+       int size = nums.size();
+       for ( first=0, sub=1; sub <= size; ) { 
+           while ( sub < size && nums[first] == nums[sub] ) sub++;
+           nums[res++] = nums[first];
+           first=sub++;
+       }
+       if ( res ) return res;
+       else return size;
+    }
+};
