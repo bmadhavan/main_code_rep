568e24ad285f903319bfd80fd17896b6fecc054d
bmadhavan <balasubramanian.madhavan@gmail.com>
2017-01-03 17:55:38 -0800

    Renaming variables and adding comments

--- CPP/Trees/DS_algo_delete_node.cpp
+++ CPP/Trees/DS_algo_delete_node.cpp
@@ -1,3 +1,4 @@
+/* 
 Given a root node reference of a BST and a key, delete the node with the given key in the BST. 
 Return the root node reference (possibly updated) of the BST.
 
@@ -14,26 +15,27 @@ Once you find the node, handle 3 cases.
 2) Node's one child is null. 
 3) Node both child are present. 
 For case 3, find the next highest node's value to be used. Change the current node value to the next highest and delete that node. 
-
+*/
 
 TreeNode* deleteNode(TreeNode* root, int key) {
         if ( !root ) return nullptr; 
-        TreeNode* temp=nullptr;
+        TreeNode* newroot=nullptr;
         
+        /* Handle all 3 cases here */
         if ( root->val == key ){ 
-            if ( root->left == root->right ) { free(root); return nullptr; }
+            /* leaf node */ 
+            if ( root->left == root->right ) { delete root; return nullptr; }
+            /* No right or left node. */ 
             if ( root->left == nullptr || root->right == nullptr ) { 
-                temp = root->left ? root->left : root->right;
-                free(root);
-                return temp; }
-                
-            /* The node has both left and right */
-            temp = root;
-            temp=temp->right;
-            while (temp->left)
-                temp=temp->left;
+                newroot = root->left ? root->left : root->right;
+                delete root;
+                return newroot;
+            }
+            /* Node has both left and right. Find the next minimum node */
+            newroot = root->right;
+            while (newroot->left) newroot=newroot->left;
             // Now delete this node
-            key = temp->val;
+            key = newroot->val;
         }
         
         if ( root->val < key ) 
@@ -41,7 +43,7 @@ TreeNode* deleteNode(TreeNode* root, int key) {
         else
             root->left = deleteNode(root->left, key);
         
-        if ( temp ) root->val=key;
+        if ( newroot ) root->val=key;
         
         return root;
     }
